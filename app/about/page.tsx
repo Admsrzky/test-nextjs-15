b7eef27ce10d99ad8325065b2741d763d4fa0324
Admsrzky <adimasrizki926@gmail.com>
2024-12-16 18:51:22 +0700

    test-nextjs-15

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+function About () {
+    return (
+        <div className="">
+            <h1 className="">About Page</h1>
+            <a href="http://localhost:3000" className="text-blue-500 underline">Home Page</a>
+        </div>
+    )
+}
+
+export default About
---
+++ components/HelloWorld.tsx
@@ -0,0 +1,10 @@
+function HelloWorld () {
+    return (
+        <div className="">
+            <h1>Hello World</h1>
+            <p>Selamat Belajar nextjs</p>
+        </div>
+    )
+}
+
+export default HelloWorld
