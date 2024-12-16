3594db94cb7a58b2debdb49d438bc07e4ad3bc2f
Adimas Rizki <129198387+Admsrzky@users.noreply.github.com>
2024-12-16 19:17:41 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
         <h1 className="text-2xl font-bold">Home Page</h1>
-        <a href="http://localhost:3000/about" className="text-blue-500 underline">About Page</a>
+        <a href="https://test-nextjs-15-mo9yyl68e-adimas-rizkis-projects.vercel.app/about" className="text-blue-500 underline">About Page</a>
     </div>
   )
 }
